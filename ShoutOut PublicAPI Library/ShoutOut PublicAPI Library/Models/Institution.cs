319088bc8ae465fce7afe301863daae9cce523f9
cristian-hanca <cristian.hanca@gmail.com>
2014-10-18 17:41:35 +0300

    PublicAPI C# Portable Library - Commit 1

---
+++ ShoutOut PublicAPI Library/ShoutOut PublicAPI Library/Models/Institution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoutOut_PublicAPI_Library.Models
+{
+    public class Institution
+    {
+
+        #region Members
+
+        private int _ID = 0;
+        public int ID { get { return _ID; } }
+
+        private string _Name = "";
+        public string Name { get { return _Name; } }
+
+        private int _CityID = 0;
+        public int CityID { get { return _CityID; } }
+
+        #endregion
+
+        #region Initialiser
+
+        public Institution()
+        { 
+        
+        }
+
+        internal Institution(int id, string name, int cityid)
+        {
+            _ID = id;
+            _Name = name;
+            _CityID = cityid;
+        }
+
+        #endregion
+
+    }
+}
